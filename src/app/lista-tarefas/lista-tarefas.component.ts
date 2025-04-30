085e6b1d6772077c34a9b98d77805cfe7b1e4f50
Wanderson Vitor Almeida <wandersonvitor96@gmail.com>
2025-04-30 00:16:57 -0300

    Inserindo estados e estilos

--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,11 +4,21 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
+import { state, style, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
-  styleUrls: ['./lista-tarefas.component.css']
+  styleUrls: ['./lista-tarefas.component.css'],
+  animations: [trigger('highlightedState', [
+    state('default', style({
+      border: '2px solid #B2B6FF'
+    })),
+    state('highlighted', style({
+      border: '4px solid #B2B6FF'
+}))
+
+])]
 })
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
