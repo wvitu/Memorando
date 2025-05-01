582c4dac18d655a620cfc78a81e8c6738d9f800b
Wanderson Vitor Almeida <wandersonvitor96@gmail.com>
2025-05-01 14:02:07 -0300

    Adicionando animações
    Aplicamos animações em elementos que não estão anexados ao DOM;
    Utilizamos os estados void e coringa;
    Melhoramos o código com os alias :enter e :leave.

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -16,3 +16,19 @@ animate(200)
 ])
 
 ])
+
+export const shownStateTrigger = trigger('shownState', [
+  transition(':enter', [
+    style ({
+      opacity: 0
+    }),
+    animate(300, style({
+      opacity: 1
+    }))
+  ]),
+  transition(':leave', [
+    animate(300, style ({
+      opacy: 0
+    }))
+  ])
+])
--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -5,13 +5,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
 import { filter } from 'rxjs';
-import { highlightedStateTrigger } from '../animations';
+import { highlightedStateTrigger, shownStateTrigger } from '../animations';
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
   styleUrls: ['./lista-tarefas.component.css'],
-  animations: [highlightedStateTrigger]
+  animations: [highlightedStateTrigger, shownStateTrigger]
 })
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
